6eb4b983201f3f9a4c5d729401879a5a7e4d2c7d
mwaboff <mwaboff@gmail.com>
2012-10-28 04:31:14 -0400

    Updated pusherPushData to new line and char data. Also edited Settings minorly

--- Plums/views.py
+++ Plums/views.py
@@ -5,20 +5,20 @@ import pusher
 import sys
 
 debug = True
+pusher.app_id = '30052'
+pusher.key = '8754603d024bc6ca00de'
+pusher.secret = '785335c8057c784eccb0'
+p = pusher.Pusher()
+
 
 def debugPrint(astring):
     if debug:
         print debug
 
-def pusherInitialize():
-    pusher.app_id = '30052'
-    pusher.key = '8754603d024bc6ca00de'
-    pusher.secret = '785335c8057c784eccb0'
-    return pusher.Pusher()
 
 def pusherPushData(idNum, actionType, lineLocation = None, dataLocation = None, dataChange=None):
     try:
-        p['%i'%idNum].trigger('dataChange', {"Type": actionType, "Location": dataLocation, "ChangeTo": dataChange})
-        debugPrint("Change Pushed: TYPE=%s LOCATION=%s CHANGE=%s" % (actionType, dataLocation, dataChange))
+        p['%s'%idNum].trigger('dataPush', {"Type": actionType, "LineFrom": lineStart, "LineTo": lineEnd, "CharFrom":charStart, "CharTo":charEnd, "ChangeTo": dataChange})
+        debugPrint("Change Pushed: ID=%s TYPE=%s LINESTART=%s LINEEND=%s CHARSTART=%s CHAREND=%s CHANGE=%s" % (idNum, actionType, lineStart, lineEnd, charStart, charEnd, dataChange))
     except e:
         print("! Unexpected error:", sys.exc_info()[0])

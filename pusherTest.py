5455a1d172cc43e7671d50e82c8f1a7c91bda548
mwaboff <mwaboff@gmail.com>
2012-10-28 04:10:29 -0400

    fixed the %d thing

--- pusherTest.py
+++ pusherTest.py
@@ -8,7 +8,7 @@ pusher.secret = '785335c8057c784eccb0'
 p = pusher.Pusher()
 
 def pusherPushData(idNum, actionType, lineStart = None, lineEnd = None, charStart = None, charEnd = None, dataChange=None):
-    p['%i'%idNum].trigger('dataPush', {"Type": actionType, "LineFrom": lineStart, "LineTo": lineEnd, "CharFrom":charStart, "CharTo":charEnd, "ChangeTo": dataChange})
+    p['%s'%idNum].trigger('dataPush', {"Type": actionType, "LineFrom": lineStart, "LineTo": lineEnd, "CharFrom":charStart, "CharTo":charEnd, "ChangeTo": dataChange})
 
     print("Change Pushed: ID=%s TYPE=%s LINESTART=%s LINEEND=%s CHARSTART=%s CHAREND=%s CHANGE=%s" % (idNum, actionType, lineStart, lineEnd, charStart, charEnd, dataChange))
 
